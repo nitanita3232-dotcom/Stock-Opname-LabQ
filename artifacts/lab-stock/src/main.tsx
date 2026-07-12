6063e2f487b8a744ff89ff660364a80288d592ac
nitanita3232-dotcom <nitanita3232@gmail.com>
2026-07-12 08:08:57 +0700

    Update main.tsx

--- artifacts/lab-stock/src/main.tsx
+++ artifacts/lab-stock/src/main.tsx
@@ -1,7 +1,9 @@
 import { createRoot } from 'react-dom/client';
-
 import App from './App';
-
 import './index.css';
+
+import { setBaseUrl } from '@workspace/api-client-react';
+
+setBaseUrl("https://workspaceapi-server-production-6236.up.railway.app");
 
 createRoot(document.getElementById('root')!).render(<App />);
